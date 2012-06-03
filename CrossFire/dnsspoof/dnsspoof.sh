f548ddf59ef43a0a62f6a39cb81c1fb5805fd189
Nexu5 <Nexu5@hush.ai>
2012-06-03 13:57:05 +0100

    Disabled firmware flash

--- CrossFire/dnsspoof/dnsspoof.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-dnsspoof -i br-lan -f /www/pineapple/config/spoofhost > /dev/null 2>/www/pineapple/logs/dnsspoof.log
--- CrossFire/urlsnarf/urlsnarf.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-urlsnarf -i br-lan >> /www/pineapple/logs/urlsnarf.log
